21c1f2fabae5038f714b8d62c6329dcffc5d6e3a
Bilge <oncekenbilge@gmail.com>
2017-06-21 18:32:16 +0300

    Code clean-up

--- a.py
+++ a.py
@@ -8,6 +8,11 @@ import cv2
 from ourfuncs import (auto_canny, four_point_transform, order_points,
                       resize_and_threshold_warped)
 
+from PIL import Image
+import PIL.ImageOps
+
+
+
 
 min_area = 5000
 min_diff = 10000
@@ -34,25 +39,43 @@ class Sign(object):
 l_arrow = Sign("Left Arrow", "ArrowL - READY.jpg")
 
 
-## image to be inspected
+
+# image = Image.open('floor.jpg')
+
+# inverted_image = PIL.ImageOps.invert(image)
+
+# inverted_image.save('inv_floor.jpg')
+
+
+# wr = cv2.imread("inv_floor.jpg", cv2.IMREAD_UNCHANGED)
+
+# cv2.imshow("popo", wr)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# ## image to be inspected
 wr = cv2.imread("raw.jpg", cv2.IMREAD_UNCHANGED)
+
+wr = cv2.imread("sample.jpeg", cv2.IMREAD_UNCHANGED)
 
 
 gray = cv2.cvtColor(wr, cv2.COLOR_BGR2GRAY)
-cv2.imshow("wr", gray)
-cv2.waitKey(0)
+# cv2.imshow("gray", gray)
 
 blurred = cv2.GaussianBlur(gray,(3,3),0)
-cv2.imshow("wr", blurred)
-cv2.waitKey(0)
+# cv2.imshow("blrred", blurred)
+
 
 edges = auto_canny(blurred)
-cv2.imshow("wr", edges)
-cv2.waitKey(0)
+# cv2.imshow("edges", edges)
+
 
 cntr_frame, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.imshow("wr", cntr_frame)
+
+cv2.imshow("cntrs", cntr_frame)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 warped_eq = None
 
@@ -67,33 +90,31 @@ warped_eq = None
 
     if unmatched pixel count is less than what we want than it is a match
 """
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    if len(approx) == 4:
-        area = cv2.contourArea(approx)
+# for cnt in contours:
+#     approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+#     if len(approx) == 4:
+#         area = cv2.contourArea(approx)
 
-        if area > min_area:
-            cv2.drawContours(wr,[approx],0,(0,0,255),1)
-            warped = four_point_transform(wr, approx.reshape(4,2), w, h)
-            warped_eq = resize_and_threshold_warped(warped)
-            diff_img = cv2.bitwise_xor(warped_eq, l_arrow.img)
-            diff = cv2.countNonZero(diff_img)
+#         if area > min_area:
+#             cv2.drawContours(wr,[approx],0,(0,0,255),1)
+#             warped = four_point_transform(wr, approx.reshape(4,2), w, h)
+#             warped_eq = resize_and_threshold_warped(warped)
+#             diff_img = cv2.bitwise_xor(warped_eq, l_arrow.img)
+#             diff = cv2.countNonZero(diff_img)
 
-            ## Checks if matches
-            if diff < min_diff:
-                # match = True
-                cv2.putText(wr,l_arrow.name, tuple(approx.reshape(4,2)[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2, cv2.LINE_AA)
-                min_area
-                break
+#             ## Checks if matches
+#             if diff < min_diff:
+#                 # match = True
+#                 cv2.putText(wr,l_arrow.name, tuple(approx.reshape(4,2)[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2, cv2.LINE_AA)
+#                 min_area
+#                 break
 
-if warped_eq.all():
-    cv2.imshow("real_sign_data", l_arrow.img)
-    cv2.imshow("warped_input",warped_eq)
-    cv2.imshow("bitwise", diff_img)
-    cv2.imshow("wr", wr)
-    cv2.waitKey(0)
-else:
-    print("found no shit")
+
+#     cv2.imshow("real_sign_data", l_arrow.img)
+#     cv2.imshow("warped_input",warped_eq)
+#     cv2.imshow("bitwise", diff_img)
+# cv2.imshow("wr", wr)
+# cv2.waitKey(0)
 
 
 
@@ -103,38 +124,41 @@ else:
 """
 
 # video = cv2.VideoCapture(0)
-# while(True):
+while(True):
 
 
-#     printscreen_pil =  ImageGrab.grab(bbox = (0,100,640,480))
-#     printscreen_numpy =   np.array(printscreen_pil, dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+    printscreen_pil =  ImageGrab.grab(bbox = (0,100,640,480))
+    printscreen_numpy =   np.array(printscreen_pil, dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
 
-#     ret, printscreen_numpy = video.read()
+    # ret, printscreen_numpy = video.read()
 
-#     gray = cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2GRAY)
-#     blurred = cv2.GaussianBlur(gray,(3,3),0)
+    gray = cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray,(3,3),0)
 
-#     edges = auto_canny(blurred)
-
-
-#     cntr_frame, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    edges = auto_canny(blurred)
 
 
-#     for cnt in contours:
-#         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-#         if len(approx) == 4:
-#             area = cv2.contourArea(approx)
+    cntr_frame, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-#             if area > min_area:
-#                 cv2.drawContours(printscreen_numpy,[approx],0,(0,0,255),2)
-#                 warped = four_point_transform(printscreen_numpy, approx.reshape(4,2), w, h)
-#                 cv2.imshow("Corrected Perspective", warped_eq)
-#                 cv2.imshow("Matching Operation", diffImg)
-#                 cv2.imshow("Contours", edges)
 
-#     cv2.imshow('window', printscreen_numpy)
-#     if cv2.waitKey(25) & 0xFF == ord('q'):
-#         video.release()
-#         cv2.destroyAllWindows()
-#         break
+    for cnt in contours:
+
+        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        if len(approx) == 4:
+            area = cv2.contourArea(approx)
+
+            if area > min_area:
+                cv2.drawContours(printscreen_numpy,[approx],0,(0,0,255),2)
+                warped = four_point_transform(printscreen_numpy, approx.reshape(4,2), w, h)
+                # cv2.imshow("Corrected Perspective", warped_eq)
+                # cv2.imshow("Matching Operation", diffImg)
+                # cv2.imshow("Contours", edges)
+
+    cv2.imshow('window', printscreen_numpy)
+    cv2.imshow("anan", cntr_frame)
+    # cv2.imshow("anan", )
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        # video.release()
+        cv2.destroyAllWindows()
+        break
 
